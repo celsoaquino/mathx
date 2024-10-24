566f56b179de92aaf9ae8b5404a1e4d3ed6ca820
celsoaquino <celso.aquino@ymail.com>
2024-10-24 11:41:12 -0300

    Initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function generateExercises(Request $request)
+    {
+
+    }
+
+    public function printExercises()
+    {
+
+    }
+
+    public function exportExercises()
+    {
+
+    }
+}
