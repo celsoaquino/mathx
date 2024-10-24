62cc3c2894f4879b3e205a53ad349a25eac60ba2
celsoaquino <celso.aquino@ymail.com>
2024-10-24 11:52:51 -0300

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index'])->name('home');
+Route::post('/generate', [MainController::class, 'generateExercises'])->name('generateExercises');
+Route::get('/print', [MainController::class, 'printExercises'])->name('printExercises');
+Route::get('/export', [MainController::class, 'exportExercises'])->name('exportExercises');
